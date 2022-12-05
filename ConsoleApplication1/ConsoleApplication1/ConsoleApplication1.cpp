4353d3096ed72df227b8d4b697f0c232f49a60a0
Арина Показацкая <pokazatskaya02@mail.ru>
2022-12-05 17:22:45 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+
+#define N 100000000
+#define blockSize 305120
+#define threads 4
+
+using std::cout;
+using std::endl;
+
+int pos = 0;
+double pi = 0.0;
+LPCRITICAL_SECTION lpCriticalSection = new CRITICAL_SECTION;
+
+DWORD WINAPI MyThreadFunction(LPVOID lpParam);
+
+int main() {
+    DWORD start, end;
+    HANDLE threadArray[threads ];
+    int position[threads ];
+    double executionSpeed = 0.0;
+    pi = 0.0;
+    InitializeCriticalSection(lpCriticalSection);
+
+    for (int i = 0; i < threads ; ++i) {
+        position[i] = blockSize * i;
+        pos = position[i];
+        threadArray[i] = CreateThread(
+            NULL,               // дескриптор защиты
+            0,                  // use default stack size  
+            MyThreadFunction,   // thread function name
+            &position[i],       // аргументы для поточной функции
+            CREATE_SUSPENDED,   // создается остановленным
+            NULL);              // returns the thread identifier
+    }
+    start = clock();
+
+    for (int i = 0; i < threads ; ++i)
+        ResumeThread(threadArray[i]);
+    WaitForMultipleObjects(threads , threadArray, TRUE, INFINITE);
+
+    pi /= (long double)N;
+    DeleteCriticalSection(lpCriticalSection);
+    end = clock();
+    executionSpeed += end - start;
+    for (int i = 0; i < threads ; ++i)
+        CloseHandle(threadArray[i]);
+
+    printf("pi: %.10lf\n", pi);
+    cout << "Execution speed: " << (long double)(executionSpeed) / CLOCKS_PER_SEC << endl;
+
+}
+
+DWORD WINAPI MyThreadFunction(LPVOID lpParam) {
+    int* first = (int*)lpParam;
+    int end = *first + blockSize;
+    long double x, tepm;
+
+    while (*first < N) {
+        tepm = 0.0;
+        for (int i = *first; (i < end) && (i < N); ++i) {
+            x = (i + 0.5) / N;
+            tepm += (4 / (1 + x * x));
+        }
+        EnterCriticalSection(lpCriticalSection);
+        pi += tepm;
+        pos += blockSize;
+        *first = pos;
+        LeaveCriticalSection(lpCriticalSection);
+        end = *first + blockSize;
+    }
+    return 0;
+}
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <omp.h>
+
+#define N 100000000
+#define blockSize 305120
+#define threads 16
+
+using std::cout;
+using std::endl;
+
+int main() {
+    DWORD start;
+    DWORD end;
+
+    long double pi = 0, executionSpeed = 0;
+    
+    start = clock();
+
+#pragma omp parallel shared(t1) reduction (+:pi) num_threads(threads)
+        {
+#pragma omp for schedule(dynamic, blockSize) nowait
+            for (int i = 0; i < N; ++i) {
+                long double x = (i + 0.5) / N;
+                pi += 4 / (1 + x * x);
+            }
+        }
+
+        pi /= (long double)N;
+        end = clock();
+        executionSpeed = end - start;
+   
+
+    cout << "pi: " << pi << endl;
+    cout << "Execution speed: " << (long double)(executionSpeed) / CLOCKS_PER_SEC << endl;
+}
